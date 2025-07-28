bd58f5ad07580cf1c20fa4ea3b1ec2a0f9376924
mpearc11 <mpearc11@jhu.edu>
2025-07-28 11:44:50 -0400

    Update module1-2streamlit.py

--- module1-2streamlit.py
+++ module1-2streamlit.py
@@ -19,28 +19,28 @@ if st.button('run blast'):
     result_stream = Blast.qblast('blastn', 'nt', target_nt_fasta)
     st.write('blast finished!')
 
-with open('myblast.xml', 'wb') as out_stream:
-    out_stream.write(result_stream.read())
-result_stream.close()
-result_stream = open('myblast.xml', 'rb')
-blast_record = Blast.read(result_stream)
+    with open('myblast.xml', 'wb') as out_stream:
+        out_stream.write(result_stream.read())
+    result_stream.close()
+    result_stream = open('myblast.xml', 'rb')
+    blast_record = Blast.read(result_stream)
 
-hit = blast_record[0]
-alignment = hit[0]
-
-@st.fragment()
-def file_download():
-    st.download_button(
-        label="Download Alignment",
-        data=str(alignment),
-        file_name='alignment.txt')
-file_download()
-
-st.code(hit)
-
-num = 1
-
-for i in blast_record[:10]:
-    st.write("HIT" + str(num))
-    st.write(i[0])
-    num = num +1
+    hit = blast_record[0]
+    alignment = hit[0]
+    
+    @st.fragment()
+    def file_download():
+        st.download_button(
+            label="Download Alignment",
+            data=str(alignment),
+            file_name='alignment.txt')
+    file_download()
+    
+    st.code(hit)
+    
+    num = 1
+    
+    for i in blast_record[:10]:
+        st.write("HIT" + str(num))
+        st.write(i[0])
+        num = num +1
