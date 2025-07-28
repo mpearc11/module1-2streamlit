1b3899044b0b75be2d7ce1dc62493162c84935ab
mpearc11 <mpearc11@jhu.edu>
2025-07-28 11:43:39 -0400

    Update module1-2streamlit.py

--- module1-2streamlit.py
+++ module1-2streamlit.py
@@ -7,21 +7,26 @@ from Bio import Blast
 Blast.email = 'mpearc11@jhu.edu'
 
 uploaded_file = st.file_uploader("Target FASTA File", type='fasta')
+
+if uploaded_file is not None:
+    st.success("FASTA file uploaded")
+else:
+    st.info("Please upload your target FASTA file")
+
 if st.button('run blast'):
     st.write('blast running...')
     target_nt_fasta = uploaded_file.read()
     result_stream = Blast.qblast('blastn', 'nt', target_nt_fasta)
     st.write('blast finished!')
 
-if uploaded_file:
-    with open('myblast.xml', 'wb') as out_stream:
-        out_stream.write(result_stream.read())
-    result_stream.close()
-    result_stream = open('myblast.xml', 'rb')
-    blast_record = Blast.read(result_stream)
+with open('myblast.xml', 'wb') as out_stream:
+    out_stream.write(result_stream.read())
+result_stream.close()
+result_stream = open('myblast.xml', 'rb')
+blast_record = Blast.read(result_stream)
 
-    hit = blast_record[0]
-    alignment = hit[0]
+hit = blast_record[0]
+alignment = hit[0]
 
 @st.fragment()
 def file_download():
