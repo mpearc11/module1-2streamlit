d9f23dcbcd650a158907774e70f8eb7c382aa130
mpearc11 <mpearc11@jhu.edu>
2025-09-07 11:10:52 -0400

    Update module1-2streamlit.py

--- module1-2streamlit.py
+++ module1-2streamlit.py
@@ -44,7 +44,7 @@ if st.button('run blast'):
     def alignment_download():
         st.download_button(
             label="Download Alignment",
-            data=str(format(alignment, 'phylip')),
+            data=str(alignment),
             file_name='alignment.txt')
     alignment_download()
     
